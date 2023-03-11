a51e77a79aa1f1956cee5ce4ef8c34523a6bb085
muharisharis13 <muharisharis13@gmail.com>
2023-03-11 14:05:25 +0700

    update href

--- components/dashboard/menu.js
+++ components/dashboard/menu.js
@@ -4,17 +4,10 @@ import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import { HelmetDashboard } from "@utils";
 
-
-const listRole = [
-  "admin",
-  "kasir",
-  "penjualan",
-  "pembelian"
-]
-
+const listRole = ["admin", "kasir", "penjualan", "pembelian"];
 
 const MenuDashboard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [menuActive, setMenuActive] = useState({
     pathname: "",
     title: "",
@@ -27,7 +20,7 @@ const MenuDashboard = () => {
         title: idxProps,
       });
       localStorage.setItem("titleSidebar", idxProps);
-      useNavigate(pathname)
+      useNavigate(pathname);
     }
   };
 
@@ -38,63 +31,80 @@ const MenuDashboard = () => {
     });
   }, []);
 
-  const btnLogout = ()=>{
-    localStorage.clear()
-    Cookies.remove("token")
-    Cookies.remove("refreshToken")
-    navigate("/auth/login")
-  }
+  const btnLogout = () => {
+    localStorage.clear();
+    Cookies.remove("token");
+    Cookies.remove("refreshToken");
+    navigate("/auth/login");
+  };
 
-  const filterRole = filter =>{
-    const role = localStorage.getItem("role")
-
-    switch (role) {
-      case "kasir":
-
-      return filter.title ==="Data Penjualan" || filter.title ==="Data Kwintansi" || filter.title==="Data Pengeluaran" 
-        
-        break;
-
-        case "penjualan":
-          return filter.title ==="Data Penjualan" || filter.title === "Cabang"
-          break;
-        case "pembelian":
-          return filter.title ==="Data Pembelian"
-          break;
-
-          case "admin":
-            return filter.title !== "Cabang" && filter.title !== "Data Pembelian" && filter.title !== "Data Kwintansi" && filter.title !== "Data Pengeluaran"
-            break;
-    
-      default:
-        break;
-    }
-  }
-
-  const filterRoleSubMenu= filter =>{
-    const role = localStorage.getItem("role")
+  const filterRole = (filter) => {
+    const role = localStorage.getItem("role");
 
     switch (role) {
       case "kasir":
+        return (
+          filter.title === "Data Penjualan" ||
+          filter.title === "Data Kwintansi" ||
+          filter.title === "Data Pengeluaran"
+        );
 
-      return filter.title ==="Penjualan" || filter.title ==="History Penjualan" || filter.title === "Kwintansi" || filter.title==="Pengeluaran"
-        
         break;
-        case "penjualan":
-          return filter.title ==="Penjualan Cabang" || filter.title === "History Penjualan Cabang"
-          break;
 
-          case "pembelian":
-            return filter.title ==="Pembelian" || filter.title === "Supplier" || filter.title === "Hitory Pembelian"
-            break;
-    
+      case "penjualan":
+        return filter.title === "Data Penjualan" || filter.title === "Cabang";
+        break;
+      case "pembelian":
+        return filter.title === "Data Pembelian";
+        break;
+
+      case "admin":
+        return (
+          filter.title !== "Cabang" &&
+          filter.title !== "Data Pembelian" &&
+          filter.title !== "Data Kwintansi" &&
+          filter.title !== "Data Pengeluaran"
+        );
+        break;
+
       default:
-        return filter
         break;
     }
-  }
+  };
 
+  const filterRoleSubMenu = (filter) => {
+    const role = localStorage.getItem("role");
 
+    switch (role) {
+      case "kasir":
+        return (
+          filter.title === "Penjualan" ||
+          filter.title === "History Penjualan" ||
+          filter.title === "Kwintansi" ||
+          filter.title === "Pengeluaran"
+        );
+
+        break;
+      case "penjualan":
+        return (
+          filter.title === "Penjualan Cabang" ||
+          filter.title === "History Penjualan Cabang"
+        );
+        break;
+
+      case "pembelian":
+        return (
+          filter.title === "Pembelian" ||
+          filter.title === "Supplier" ||
+          filter.title === "Hitory Pembelian"
+        );
+        break;
+
+      default:
+        return filter;
+        break;
+    }
+  };
 
   return (
     <aside
@@ -107,7 +117,6 @@ const MenuDashboard = () => {
           <span className="app-brand-logo demo">
             <img src="/assets/logo/logo.png" alt="/assets/logo/logo.png" />
           </span>
-         
         </a>
 
         <a
@@ -139,7 +148,7 @@ const MenuDashboard = () => {
               href={
                 item.href === "javascript:void(0);"
                   ? "javascript:void(0);"
-                  : ""
+                  : item.href
               }
               onClick={() => handleMenu(item.href, item.title)}
               className={
@@ -153,35 +162,41 @@ const MenuDashboard = () => {
             </a>
             {item["sub-menu"]?.length > 0 ? (
               <ul className="menu-sub">
-                {item["sub-menu"]?.filter(filterRoleSubMenu)?.map((itemSub, idxSub) => (
-                  <li
-                    key={idxSub}
-                    className={
-                      menuActive.pathname === location.pathname &&
-                      menuActive.title === itemSub.title
-                        ? "menu-item active"
-                        : "menu-item"
-                    }
-                  >
-                    <a
-                      href={itemSub.href}
-                      onClick={() => handleMenu(itemSub.href, itemSub.title)}
-                      className="menu-link"
+                {item["sub-menu"]
+                  ?.filter(filterRoleSubMenu)
+                  ?.map((itemSub, idxSub) => (
+                    <li
+                      key={idxSub}
+                      className={
+                        menuActive.pathname === location.pathname &&
+                        menuActive.title === itemSub.title
+                          ? "menu-item active"
+                          : "menu-item"
+                      }
                     >
-                      <div data-i18n="Account">{itemSub.title}</div>
-                    </a>
-                  </li>
-                ))}
+                      <a
+                        href={itemSub.href}
+                        onClick={() => handleMenu(itemSub.href, itemSub.title)}
+                        className="menu-link"
+                      >
+                        <div data-i18n="Account">{itemSub.title}</div>
+                      </a>
+                    </li>
+                  ))}
               </ul>
             ) : null}
           </li>
         ))}
         <li class="menu-item">
-              <a href="javascript:void(0);" onClick={btnLogout} className="menu-link">
-                <i className="menu-icon tf-icons bx bx-power-off"></i>
-                <div data-i18n="Analytics">Logout</div>
-              </a>
-            </li>
+          <a
+            href="javascript:void(0);"
+            onClick={btnLogout}
+            className="menu-link"
+          >
+            <i className="menu-icon tf-icons bx bx-power-off"></i>
+            <div data-i18n="Analytics">Logout</div>
+          </a>
+        </li>
       </ul>
     </aside>
   );
